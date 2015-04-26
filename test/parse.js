04560cc4303fd05940de9d62b18fa34dbae5ae13
Shawn Allen <shawn.allen@gsa.gov>
2015-04-26 11:29:30 -0700

    parse options with yargs

--- test/parse.js
+++ test/parse.js
@@ -1,8 +1,9 @@
-var table = require('../'),
-    ndjson = require('ndjson');
+var yargs = require('yargs');
+var options = yargs.argv;
+var table = require('../');
+var ndjson = require('ndjson');
 
 process.stdin
-  .pipe(table.parse({
-  }))
+  .pipe(table.parse(options))
   .pipe(ndjson.stringify())
   .pipe(process.stdout);
